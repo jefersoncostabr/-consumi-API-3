c661e1778eccedfe21e7521f5d4f0779bb8b3d28
jeferson <jefersonb88@gmail.com>
2024-05-10 09:11:06 -0300

    tentativa de correção excluir e cadastrar

--- src/botoes.js
+++ src/botoes.js
@@ -1,11 +1,10 @@
-import { endpoint3, endpoint4, fetchData, fetchPostData } from "./scriptMain.js"
+import { endpoint3, endpoint4, fetchData, fetchPostData, deleteMsg } from "./scriptMain.js"
 
 // Exibir totos
 const btnExibir = document.getElementById("btnExibir"); 
 btnExibir.addEventListener('click', () =>{
     fetchData(endpoint3)
 });
-
 
 // cadastrar
 const btnExibirPID = document.getElementById("btnCadastrar"); 
@@ -14,8 +13,8 @@ btnExibirPID.addEventListener('click', () =>{
     let varMsg = prompt("Mensagem")
 
     let novaMsgComPrompt = {
-        nome: varNome,
-        mensagem: varMsg,
+        // nome: varNome,
+        mensagem: varMsg
     }
     fetchPostData(endpoint4, novaMsgComPrompt);
 });
@@ -23,6 +22,6 @@ btnExibirPID.addEventListener('click', () =>{
 // Excluir Específico
 const btnExcluir = document.getElementById("btnExcluirPorId"); 
 btnExcluir.addEventListener('click', () =>{
-    let idEscolhido = prompt("Digite o numero do item a excluir").
-    deleteMsg(idEscolhido)
+    let idEscolhido = prompt("Digite o numero do item a excluir")
+    return deleteMsg(idEscolhido)
 });
